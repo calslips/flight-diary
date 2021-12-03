4d3fe7330125e8a584d21db089815caa2e86b181
Caleb Phillips <csphillips5@gmail.com>
2021-12-02 19:24:05 -0500

    refactor service according to changes made to type the diary data

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -2,8 +2,6 @@ import diaries from '../../data/diaries';
 
 import { DiaryEntry } from '../types';
 
-const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
-
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
 };
