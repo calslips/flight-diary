34cc84d6b74ef9474fc69935a41ba842536e4316
Caleb Phillips <csphillips5@gmail.com>
2021-12-02 19:07:57 -0500

    create file to define all types for the project

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+
+export type Visibility = 'greate' | 'good' | 'ok' | 'poor';
+
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
